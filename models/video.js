f326605475bd9312709051d3c0e5a87bfff2f1a8
oladips <oladips200@gmail.com>
2022-09-25 22:10:04 +0000

    created video model

---
+++ models/video.js
@@ -0,0 +1,47 @@
+/** @format */
+import mongoose from "mongoose";
+
+const VideoSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    dislikes: {
+      type: [String],
+      default: [],
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model("Video", VideoSchema);
+export default Video;
